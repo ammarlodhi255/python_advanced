53b88f11641d8dfc107d62297e74c1f6b5fc5b59
Ammar Ahmed <ammarahmed@Ammars-MacBook-Pro.local>
2024-01-14 02:46:26 +0500

    add file

---
+++ everything_that_evaluates_to_false.py
@@ -0,0 +1,19 @@
+
+'''
+ Following values evaluate to false in python:
+
+ False 
+ 0 of any numeric type.
+ None
+ Any empty sequence. For example, '', (), []
+ Any empty mapping. For example, {}
+'''
+
+if not None:
+    print('Here')
+
+if not []:
+    print('Here2')
+
+if not 0:
+    print('Here3')
